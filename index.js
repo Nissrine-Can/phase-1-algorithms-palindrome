e2aa2ca63661158ad0ed0d1a77eb19a615bb39f8
Nissrine Canina <niss.canina@gmail.com>
2021-10-01 20:05:34 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
+let reversedWord;
 function isPalindrome(word) {
-  // Write your algorithm here
+  
+     reversedWord = word.split("").reverse().join("").toLowerCase()
+    word = word.toLowerCase()
+        if (word === reversedWord) {
+          return true
+        } else {
+          return false
+        }
 }
 
+
 /* 
-  Add your pseudocode here
+  Add your pseudocode here:
+
+  Step1: reverse a string => (split, reverse, join)
+  Step2: convert string and reversed string to lowercase
+  Step3: if (string === reversedString) {
+    return true
+  } else return false
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here:
+
+  The Palindrome function is returning true when a string reads the same forwards and backwards. First, the function main task is to reverse the string. Since the reverse() built in Javascript method works only on arrays, we split the string characters using split() which will transform the string into an array. After that, the reverse method can be applied then we join the array back into a string using join(). Then, the string received as an argument as well as the reversed one should be all in lower case to avoid case sensitivity. Next, a conditional statment will evaluate if the reverse string matches the original string.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("Madam"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("saippuaKIvikauppias"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("palindrome"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
